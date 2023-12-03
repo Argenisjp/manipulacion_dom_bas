25c253323f57d7fe39dd9e27dc6aaa65d322db17
camilo <camilo.castillo@gopass.com.co>
2023-12-02 19:51:21 -0500

    practica manipulacion del DOM javaScript

---
+++ ejem.js
@@ -0,0 +1,59 @@
+
+ let suma = document.getElementById('suma').value;
+ let resta = document.getElementById('resta').value;
+ let multiplicacion = document.getElementById('multiplicacion').value;
+ let division = document.getElementById('division').value;
+
+ 
+ 
+ //  console.log(suma)
+ //  console.log(resta)
+ //  console.log(multiplicacion)
+ //  console.log(division)
+ 
+ 
+ function sumar(){
+   
+   let numero1 = parseInt(document.getElementById('num1').value);
+   let numero2 = parseInt(document.getElementById('num2').value);
+  
+   let resul = numero1 + numero2;
+   console.log(resul); 
+
+  
+}
+
+
+ 
+function restar(){
+   
+  let numero1 = parseInt(document.getElementById('num1').value);
+  let numero2 = parseInt(document.getElementById('num2').value);
+
+ let resul = numero1 - numero2;
+ console.log(resul); 
+
+
+}
+
+function multiplicar(){
+   
+  let numero1 = parseInt(document.getElementById('num1').value);
+  let numero2 = parseInt(document.getElementById('num2').value);
+
+ let resul = numero1 * numero2;
+ console.log(resul); 
+
+
+}
+
+function dividir(){
+   
+  let numero1 = parseInt(document.getElementById('num1').value);
+  let numero2 = parseInt(document.getElementById('num2').value);
+
+ let resul = numero1 / numero2;
+ console.log(resul); 
+
+
+}
